5ddbf1634085849c77a7328f8effa7861afb4d77
Anshul Chaudhary <anshulch.1606@gmail.com>
2025-12-24 04:24:09 +0530

    Implementation of Interface

---
+++ Day4_OOPS_Practice/Interface.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Day4_OOPS_Practice
+{
+    public interface IPrint
+    {
+         public void print();
+    }
+
+    public class Functionality : IPrint
+    {
+        public void print()
+        {
+            Console.WriteLine("Printing Done");
+        }
+    }
+    public class Interface
+    {
+        public static void Main(String[] args)
+        {
+            Functionality printFunction = new Functionality();
+            printFunction.print();
+        }
+    }
+}
