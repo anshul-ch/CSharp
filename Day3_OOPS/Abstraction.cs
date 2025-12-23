8d5e481155b92cb7f375dbe603d92c7ed8f5ea94
Anshul Chaudhary <anshulch.1606@gmail.com>
2025-12-23 06:01:00 +0530

    Abstraction Implementation

---
+++ Day3_OOPS/Abstraction.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CSharp.Day3_OOPS
+{
+
+    public abstract class TaxCalculate
+    {
+        public string empName { get; set; }
+        public int income { get; set; }
+
+        public abstract double CaculateTax();
+
+        public void Display()
+        {
+            Console.WriteLine("Emp name: " + empName);
+            Console.WriteLine("Incone : " + income);
+            Console.WriteLine("Tax payed: " + CaculateTax());
+
+        }
+    }
+
+    public class IndianTax : TaxCalculate
+    {
+        public override double CaculateTax()
+        {
+            return income * 0.20;
+        }
+    }
+    public class UStax : TaxCalculate
+    {
+        public override double CaculateTax()
+        {
+            return income * 0.30;
+        }
+    }
+
+    public class Abstraction
+    {
+        public static void Main(String[] args)
+        {
+            IndianTax empIndia = new IndianTax() { empName = "Anuj", income = 1000000 };
+            empIndia.CaculateTax();
+            UStax empUS = new UStax() { empName = "james", income = 250000 };
+            empUS.CaculateTax();
+            empUS.Display();
+            empIndia.Display();
+        }
+    }
+}
+
+
+
