2909864e19ea9d8db2634610c86fb187a893226c
Anshul Chaudhary <anshulch.1606@gmail.com>
2025-12-26 04:13:40 +0530

    Multiple Inheritance

---
+++ Day4_OOPS_Practice/MultipleInheritance.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CSharp.Day4_OOPS_Practice
+{
+
+    public interface Vegday
+    {
+        void RequiredItemsVeg();
+        public void Taste();
+    }
+
+    public interface NonVegday
+    {
+        void RequiredItemsNonVeg();
+        void Taste();
+    }
+
+    public class Eating : Vegday, NonVegday
+    {
+        public void RequiredItemsVeg() 
+        {
+            Console.WriteLine("It is a veg day");
+        }
+         
+        
+        public void RequiredItemsNonVeg() 
+        {
+            Console.WriteLine("It is a non veg day");
+        }
+
+        void Vegday.Taste()
+        {
+            Console.WriteLine("Veg");
+        }
+        void NonVegday.Taste()
+        {
+            Console.WriteLine("Nonveg");
+        }
+    }
+    public class MultipleInheritance
+    {
+        public static void Main(string[] args)
+        {
+            Eating eating = new Eating();
+            ((Vegday) eating).Taste();
+            ((NonVegday)eating).Taste();
+            
+        }
+    }
+}
