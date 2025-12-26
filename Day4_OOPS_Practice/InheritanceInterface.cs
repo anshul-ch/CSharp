9e1c7320cbd18c3304b2e1bb74a09bc68c29f60f
Anshul Chaudhary <anshulch.1606@gmail.com>
2025-12-26 07:04:30 +0530

    Inheritance practice with Interface

---
+++ Day4_OOPS_Practice/InheritanceInterface.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Day4_OOPS_Practice
+{
+    /// <summary>
+    /// This program implements the usage of interface to implemetn multiple inheritance.
+    /// </summary>
+    interface SingingBird
+    {
+        void BirdSings();
+        void BirdDance();
+    }
+    interface FlyingBird
+    {
+        void BirdFlys();
+        void BirdWalk();
+    }
+
+    class Bird : SingingBird, FlyingBird
+    {
+        public void BirdSings()
+        {
+            Console.WriteLine("This bird Sings");
+        }
+        public void BirdDance()
+        {
+            Console.WriteLine("Bird Dance");
+        }
+        public void BirdWalk()
+        {
+            Console.WriteLine("Bird Walks");
+        }
+        public void BirdFlys()
+        {
+            Console.WriteLine("This bird flys");
+        }
+    }
+        public class InheritanceInterface
+        {
+            public static void Main(String[] args)
+            {
+                Bird bird = new Bird();
+                ((FlyingBird)bird).BirdFlys();
+                ((FlyingBird)bird).BirdWalk();
+                ((SingingBird)bird).BirdDance();
+                ((SingingBird)bird).BirdSings();
+            }
+        }
+}
