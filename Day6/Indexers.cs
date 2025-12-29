567f5306ebf29e09c38bdacf91a448c1f7886b07
Anshul Chaudhary <anshulch.1606@gmail.com>
2025-12-29 10:05:42 +0530

    Indexers

---
+++ Day6/Indexers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CSharp.Day6
+{
+	public class MyData
+	{
+		private string[] data = new string[3];
+		public string this[int index]
+		{
+			get { return data[index]; }
+			set { data[index] = value; }
+        }
+    }
+	public class Indexers
+	{
+		public static void Main(String[] args)
+		{
+			MyData myData = new MyData();
+			myData[0] = "First";
+			myData[1] = "Second";
+			myData[2] = "Third";
+			Console.WriteLine(myData[0]);
+			Console.WriteLine(myData[1]);
+			Console.WriteLine(myData[2]);
+		}
+    }
+}
