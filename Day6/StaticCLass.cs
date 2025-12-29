f51d666995338b79a1fbb57c0e3a46633d3f7003
Anshul Chaudhary <anshulch.1606@gmail.com>
2025-12-29 12:49:20 +0530

    Static Classes

---
+++ Day6/StaticCLass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Day6
+{
+    /// <summary>
+    /// Static CLass :A static class is a class that cannot be instantiated, 
+    /// meaning you cannot use the new keyword to create an object from it
+    /// <remark>In a Static Class, all the members must be static and it can be called in a static class only.
+    /// There is no need to create objects for static class, we can directly access the meber functions.
+    /// </remark>
+    /// </summary>
+    public static class GeneralUse
+    {
+        static GeneralUse()
+        {
+
+        }
+        public static int GetRollno(int rollno)
+        {
+            return rollno;
+        }
+    }
+    public class StaticCLass
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine(GeneralUse.GetRollno(10));
+        }
+    }
+}
