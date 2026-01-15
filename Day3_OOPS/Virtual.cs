4ed5cda8b5e21bde70ede8c536377165dc3353c1
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-01-15 14:31:17 +0530

    practice

--- Day3_OOPS/Virtual.cs
+++ Day3_OOPS/Virtual.cs
@@ -7,28 +7,28 @@ namespace CSharp.Day3_OOPS
     /// <summary>
     /// This program implements the use of Virtual and Override for method overriding
     /// </summary>
-
     public class MethodOverride
     {
-        public virtual string GetInfo()            // virtual allow you to overide the function
+        public virtual string GetInfo() // virtual allow you to overide the function
         {
             return "I like to play cricket";
         }
     }
-    public class MethodOverride2 : MethodOverride 
+
+    public class MethodOverride2 : MethodOverride
     {
-        public override string GetInfo()             // override actually override the function
+        public override string GetInfo() // override actually override the function
         {
             return "I like to play PC games";
         }
     }
+
     public class Virtual
     {
         public static void Main(string[] args)
         {
             MethodOverride2 override1 = new MethodOverride2();
             Console.WriteLine(override1.GetInfo());
-            
         }
     }
 }
