0d98efc199c7d6c34874ee3103b2e45ecbb60343
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-01-28 13:03:25 +0530

    Gets the total rent based on filteration

---
+++ Practice/Apartment/Apartment_Invent.cs
@@ -0,0 +1,59 @@
+﻿namespace CSharp.Practice.Apartment;
+
+class ApartmentDetails
+{
+    private string apartmentName { get; set; }
+    private double apartmentRent { get; set; }
+    private Dictionary<string, double> apartmentMap = new Dictionary<string, double>();
+    public void addDepartmentDetails(string apartmentID, double rentAmount)
+    {
+       apartmentName = apartmentID;
+       apartmentRent = rentAmount;
+        apartmentMap[apartmentName] = apartmentRent;
+    }
+
+    public double TotalAmountRentBetweenRange(double minREnt, double maxRent)
+    {
+        double sum = 0;
+        foreach (var apartment in apartmentMap)
+        {
+            if (apartment.Value >= minREnt && apartment.Value <= maxRent)
+            {
+                sum += apartment.Value;
+            }
+        }
+
+        return sum;
+    }
+}
+
+public class Apartment_Invent
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Enter the number of apartments to be added:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the details as ApartName:RentAmount");
+        ApartmentDetails apartment = new ApartmentDetails();
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+            string[] parts = input.Split(':');
+            string apartname = parts[0];
+            double rentamount = double.Parse(parts[1]);
+            apartment.addDepartmentDetails(apartname, rentamount);
+        }
+        Console.WriteLine("Enter the inimum and maximum rent amount to filter:");
+        double minrent = double.Parse(Console.ReadLine());
+        double maxrent = double.Parse(Console.ReadLine());
+        double totalRent = apartment.TotalAmountRentBetweenRange(minrent, maxrent);
+        if (totalRent > 0)
+        {
+            Console.WriteLine("Total rent amount between the given range: " + totalRent);
+        }
+        else
+        {
+            Console.WriteLine("No apartments found in the given range.");
+        }
+    }
+}
