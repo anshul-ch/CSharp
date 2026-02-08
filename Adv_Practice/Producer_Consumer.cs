42e8f0c7e53a8c2b9f7e93e12baf340030d6646a
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-02-08 18:04:33 +0530

    producer consumer problem using queue

---
+++ Adv_Practice/Producer_Consumer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace CSharp.Adv_Practice
+{
+    public class Order
+    {
+        public int Id { get; set; }
+    }
+    public class OrderQueueProcessor
+    {
+        private readonly Queue<Order> queue = new Queue<Order>();
+
+        public void AddOrder(Order order)
+        {
+            queue.Enqueue(order);
+        }
+
+        public int ProcessAll()
+        {
+            int count = 0;
+            while (queue.Count > 0)
+            {
+                queue.Dequeue();
+                count++;
+            }
+            return count;
+        }
+    }
+
+    [TestFixture]
+    public class OrderQueueTest
+    {
+        [Test]
+        public void AllOrdersAreProcessed()
+        {
+            var processor = new OrderQueueProcessor();
+            processor.AddOrder(new Order { Id = 1 });
+            processor.AddOrder(new Order { Id = 2 });
+
+            Assert.That(processor.ProcessAll(), Is.EqualTo(2));
+        }
+    }
+
+    class Producer_Consumer
+    {
+        static void Main() { }
+    }
+}
