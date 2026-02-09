5d979ef8dec60bceccdd25cb594a45479ecdd855
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-02-09 11:46:57 +0530

    Static ticket count

---
+++ Scenario_based_questions/Ticket_Booking.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Scenario_based_questions
+{
+    class Ticket_Book
+    {
+        public static int TicketCount = 1000;
+        public void AddTicket()
+        {
+            TicketCount++;
+        }
+        public void PrintDetails()
+        {
+            Console.WriteLine("TotalTickets: " + TicketCount);
+        }
+    }
+    public class Ticket_Booking
+    {
+        static void Main(String[] args)
+        {
+            Ticket_Book booking = new Ticket_Book();
+            booking.AddTicket();
+            booking.AddTicket();
+            booking.AddTicket();
+            booking.AddTicket();
+            booking.PrintDetails();
+            booking.AddTicket();
+            booking.AddTicket();
+            booking.PrintDetails();
+        }
+    }
+}
