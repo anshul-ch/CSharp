e42c38640f93578774fdb13092b2cbf5664f6d14
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-02-09 13:45:38 +0530

    ref variable use

---
+++ Scenario_based_questions/Wallet_Payment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp.Scenario_based_questions
+{
+    class Wallet_Payment
+    {
+        static void MakePayment(ref double walletBalance, double amount)
+        {
+            if (amount > 0 && amount <= walletBalance)
+            {
+                walletBalance -= amount;
+            }
+        }
+
+        static void Main()
+        {
+            double walletBalance = Convert.ToDouble(Console.ReadLine());
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            MakePayment(ref walletBalance, amount);
+
+            Console.WriteLine(walletBalance);
+        }
+    }
+}
