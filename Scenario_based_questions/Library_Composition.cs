c03e1335c993279981ad012a17160fd3afecd1ce
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-02-09 11:59:42 +0530

    object relation

---
+++ Scenario_based_questions/Library_Composition.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Scenario_based_questions
+{
+    public class Author
+    {
+        public string AuthorName { get; set; }
+        public string Country { get; set; }
+    }
+    public class Book
+    {
+        public string? Title { get; set; }
+        public int Price { get; set; }
+        public Author? authorDetails;
+        public Book(string title, int price, Author author)
+        {
+            Title = title;
+            Price = price;
+            authorDetails = author;
+        }
+        public void PrintDetails()
+        {
+            Console.WriteLine("Author Name: " + authorDetails.AuthorName);
+            Console.WriteLine("Author Country: " + authorDetails.Country);
+            Console.WriteLine("Price: " + Price);
+            Console.WriteLine("Title: " + Title);
+        }
+    }
+    public class Library_Composition
+    {
+        static void Main(String[] args)
+        {
+            Book book = new Book("Interstellar", 400, new Author
+            {
+                AuthorName = "Heinsberg",
+                Country = "USA"
+            });
+            book.PrintDetails();
+        }
+    }
+}
