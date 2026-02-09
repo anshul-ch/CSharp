878b30aed4c01242d2559616666daa6044e1e9ed
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-02-09 13:50:44 +0530

    avg, min and max of sales

---
+++ Scenario_based_questions/SalesAnalysis.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class SalesAnalysis
+{
+    static void Main()
+    {
+        int[] sales = new int[7];
+
+        for (int i = 0; i < 7; i++)
+        {
+            sales[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int max = sales[0];
+        int min = sales[0];
+        int sum = 0;
+
+        for (int i = 0; i < sales.Length; i++)
+        {
+            sum += sales[i];
+
+            if (sales[i] > max)
+            {
+                max = sales[i];
+            }
+
+            if (sales[i] < min)
+            {
+                min = sales[i];
+            }
+        }
+
+        double avg = (double)sum / sales.Length;
+
+        Console.WriteLine("Highest Sale: " + max);
+        Console.WriteLine("Lowest Sale: " + min);
+        Console.WriteLine("Average Sale: " + avg);
+    }
+}
