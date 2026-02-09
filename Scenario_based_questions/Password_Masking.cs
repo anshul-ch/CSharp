8fe67680b24f650c20831597d8e6fbfc9ad80e4e
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-02-09 12:10:56 +0530

    password masking

---
+++ Scenario_based_questions/Password_Masking.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace CSharp.Scenario_based_questions
+{
+    class Password_generate
+    {
+        public string password { get; set; }
+        public Password_generate(string pass)
+        {
+            password = pass;
+        }
+        public void Pass_Manupilation()
+        {
+            if(string.IsNullOrEmpty(password) || password.Length < 3)
+            {
+                Console.WriteLine(password);
+                return;
+            }
+            char[] passArray = password.ToCharArray();
+            for(int i = 1; i < passArray.Length - 1; i++)
+            {
+                passArray[i] = '*';
+            }
+            string result = new string(passArray);
+            Console.WriteLine(result);
+
+        }
+    }
+    public class Password_Masking
+    {
+        static void Main(String[] args)
+        {
+            Password_generate password = new Password_generate("Welcome");
+            password.Pass_Manupilation();
+        }
+    }
+}
