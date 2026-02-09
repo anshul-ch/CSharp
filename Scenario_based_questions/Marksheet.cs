7fc76bfe158bdb764cc94d865b08f2423f08fc00
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-02-09 13:48:01 +0530

    out variable use

---
+++ Scenario_based_questions/Marksheet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CSharp.Scenario_based_questions
+{
+    class Marksheet
+    {
+        static void CalculateResult(
+            int[] marks,
+            out int total,
+            out double avg,
+            out string result)
+        {
+            total = 0;
+            bool isPass = true;
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                total += marks[i];
+
+                if (marks[i] < 35)
+                {
+                    isPass = false;
+                }
+            }
+
+            avg = (double)total / marks.Length;
+            result = isPass ? "Pass" : "Fail";
+        }
+
+        static void Main()
+        {
+            int[] marks = { 78, 65, 40, 55, 90 };
+
+            CalculateResult(marks, out int total, out double avg, out string result);
+
+            Console.WriteLine("Total: " + total);
+            Console.WriteLine("Average: " + avg);
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
