b5ae5283601ff25a192284077ed7c981cabd90f4
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-02-09 13:52:38 +0530

    Remove duplicates

---
+++ Scenario_based_questions/Remove_Duplicates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Remove_Duplicates
+{
+    static void Main()
+    {
+        int[] data = { 4, 5, 4, 3, 5, 2, 3 };
+        int[] unique = new int[data.Length];
+        int count = 0;
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            bool exists = false;
+
+            for (int j = 0; j < count; j++)
+            {
+                if (unique[j] == data[i])
+                {
+                    exists = true;
+                    break;
+                }
+            }
+
+            if (!exists)
+            {
+                unique[count] = data[i];
+                count++;
+            }
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(unique[i] + " ");
+        }
+    }
+}
