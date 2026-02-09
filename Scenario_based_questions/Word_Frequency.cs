24c56109e0f3593438dd6657bca3324b05d8be1a
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-02-09 13:10:55 +0530

    calculate word frequency

---
+++ Scenario_based_questions/Word_Frequency.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp.Scenario_based_questions
+{
+    class Frequency
+    {
+        public string sentence { get; set; }
+
+        public Frequency(string input)
+        {
+            sentence = input;
+        }
+
+        public Dictionary<string, int> CalculateFrequency()
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            if (string.IsNullOrWhiteSpace(sentence))
+            {
+                return result;
+            }
+
+            string[] words = sentence.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string word in words)
+            {
+                if (result.ContainsKey(word))
+                {
+                    result[word]++;
+                }
+                else
+                {
+                    result[word] = 1;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public class Word_Frequency
+    {
+        static void Main(string[] args)
+        {
+            Frequency freq = new Frequency("Dot net dot NET core Core");
+
+            Dictionary<string, int> output = freq.CalculateFrequency();
+
+            foreach (var item in output)
+            {
+                Console.WriteLine(item.Key + " : " + item.Value);
+            }
+        }
+    }
+}
