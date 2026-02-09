97d268283e1a7ccf505d6e7b484118d994faf635
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-02-09 13:24:01 +0530

    longest word in a paragraph

---
+++ Scenario_based_questions/Longest_Word.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CSharp.Scenario_based_questions
+{
+    class Word
+    {
+        public string Paragraph { get; set; }
+        public Word(string para)
+        {
+            Paragraph = para;
+        }
+        public void CalculateLongestWord()
+        {
+            if (string.IsNullOrEmpty(Paragraph))
+            {
+                Console.WriteLine(Paragraph);
+            }
+            
+            string[] data = Regex.Matches(Paragraph, @"[A-Za-z]+")
+                                .Select(m => m.Value)
+                                .ToArray();
+
+            Array.Sort(data, (a, b) => b.Length.CompareTo(a.Length));
+
+            Console.WriteLine(data[0]);
+        }
+    }
+    public class Longest_Word
+    {
+        public static void Main(String[] args) {
+            Word word = new Word("Elephant, hello, ABCDEFDSVASD:: HI AAAAAAAAAAAAAAAA");
+            word.CalculateLongestWord();
+    }
+    }
+}
