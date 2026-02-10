82a3e66dd85c0fe59ce87c71f584da690c2b669f
Anshul Chaudhary <anshulch.1606@gmail.com>
2026-02-10 11:51:21 +0530

    Using reflections to invoke methods

---
+++ Additional_Topics/Reflections_Invoke.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+
+namespace CSharp.Additional_Topics
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public decimal Salary { get; private set; }
+
+        private string secretCode = "X9Z";
+
+        public Employee() { }
+
+        public Employee(int id, string name, decimal salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void GiveRaise(decimal amount)
+        {
+            Salary += amount;
+        }
+
+        private string GetSecretCode() => secretCode;
+    }
+    class Reflections_Invoke
+    {
+        static void Main()
+        {
+            Employee emp = new Employee(101, "Arun", 45000m);
+
+            Type t = emp.GetType();
+            MethodInfo? m = t.GetMethod("GiveRaise");
+            MethodInfo? m1 = t.GetMethod("GetSecretCode", BindingFlags.NonPublic | BindingFlags.Instance);
+           object? secret =  m1!.Invoke(emp,null);
+            m!.Invoke(emp, new object[] { 5000m });
+            Console.WriteLine(secret);
+            Console.WriteLine(emp.Salary);
+        }
+    }
+}
